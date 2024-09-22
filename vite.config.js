d8204c8593f1b4491362ca8bb8508881c0dba8a4
truman <trumanzhe@gmail.com>
2024-09-22 03:45:08 -0400

    home page chanage

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/tmanzhe.github.io/', // Note the leading slash
+  base: '/tmanzhe.github.io/', // Keep the leading slash
   plugins: [react()],
-  assetsInclude: ['**/*.JPG'],
+  assetsInclude: ['**/*.JPG', '**/*.jpg'], // Include both uppercase and lowercase
+
 });
